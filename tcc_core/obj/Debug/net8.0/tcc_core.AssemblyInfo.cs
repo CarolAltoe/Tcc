ad24373aa4e1189bfff896c4aba308793cb9af23
maria.broedel <117489045+CarolAltoe@users.noreply.github.com>
2024-08-20 12:56:44 -0300

    Docker test

--- tcc_core/obj/Debug/net8.0/tcc_core.AssemblyInfo.cs
+++ tcc_core/obj/Debug/net8.0/tcc_core.AssemblyInfo.cs
@@ -11,10 +11,11 @@
 using System;
 using System.Reflection;
 
+[assembly: Microsoft.Extensions.Configuration.UserSecrets.UserSecretsIdAttribute("94536cf0-adad-4f33-af10-5e6479d4b556")]
 [assembly: System.Reflection.AssemblyCompanyAttribute("tcc_core")]
 [assembly: System.Reflection.AssemblyConfigurationAttribute("Debug")]
 [assembly: System.Reflection.AssemblyFileVersionAttribute("1.0.0.0")]
-[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+b36932ffe80131ee29006fe5c3a87f14c187459f")]
+[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+e8d153e3efa6ba63600c4de263314f5fd72db889")]
 [assembly: System.Reflection.AssemblyProductAttribute("tcc_core")]
 [assembly: System.Reflection.AssemblyTitleAttribute("tcc_core")]
 [assembly: System.Reflection.AssemblyVersionAttribute("1.0.0.0")]
